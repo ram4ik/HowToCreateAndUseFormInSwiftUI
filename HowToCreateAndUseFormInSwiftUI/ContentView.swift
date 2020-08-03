d6294ffcf23550ddc12ee5e86d4a7dfbbec5ef95
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-03 13:00:18 +0300

    init

--- HowToCreateAndUseFormInSwiftUI/ContentView.swift
+++ HowToCreateAndUseFormInSwiftUI/ContentView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name: String = ""
+    @State private var phoneNumber: String = ""
+    @State private var isActive: Bool = true
+    @State private var notificationEnable: Bool = false
+    @State private var indexPicker = 0
+    var optionPicker = ["Always", "While Using", "When Unlocked", "Never"]
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            Form {
+                Section(header: Text("User Information")) {
+                    TextField("Name", text: $name)
+                    TextField("Phone Number", text: $phoneNumber)
+                    Toggle(isOn: $isActive, label: {
+                        Text("Account is active")
+                    })
+                }
+                Section(header: Text("Notification")) {
+                    Toggle(isOn: $notificationEnable) {
+                        Text("Enabled")
+                    }
+                    Picker(selection: $indexPicker, label: Text("Picker")) {
+                        ForEach(0 ..< optionPicker.count) {
+                            Text(self.optionPicker[$0])
+                        }
+                    }
+                }
+                Section(header: Text("About")) {
+                    HStack {
+                        Text("Version")
+                        Spacer()
+                        Text("1.0.1")
+                    }
+                }
+                Section(header: Text("Reset Date")) {
+                    Button(action: {
+                        print("Action execute")
+                    }, label: {
+                        Text("Reset All Cache")
+                    })
+                }
+            }.navigationBarTitle("Settings")
+        }
     }
 }
 
