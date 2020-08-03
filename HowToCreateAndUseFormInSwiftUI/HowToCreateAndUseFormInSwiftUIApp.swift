88f3b4fa30cf0d3440f27393562523d24598a607
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-03 12:36:22 +0300

    Initial Commit

---
+++ HowToCreateAndUseFormInSwiftUI/HowToCreateAndUseFormInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateAndUseFormInSwiftUIApp.swift
+//  HowToCreateAndUseFormInSwiftUI
+//
+//  Created by ramil on 03.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateAndUseFormInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
